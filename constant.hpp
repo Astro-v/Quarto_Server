15374f4a0c7bfcf06d2a27dcc1418cac3c152957
Astro-v <valentinmichel07200@gmail.com>
2021-10-26 18:37:20 +0200

    First commit

---
+++ constant.hpp
@@ -0,0 +1,8 @@
+#ifndef __CONSTANT_HPP__
+#define __CONSTANT_HPP__
+
+/*---- SERVER ----*/
+#define SERVER_PORT 5500	
+#define SERVER_ADRESS "90.39.145.229" // IP adress of the server
+
+#endif // __CONSTANT_HPP__
---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+    ################ QUARTO ################
+    MICHEL VALENTIN
+    -- SERVER --
+    Octobre 2021
+    ################ END ################
+*/
+
+// LIBRARY
+#include <iostream>
+
+// SFML
+
+// LOCAL FILE
+
+using namespace std;
+
+int main(){
+    return 0;
+}
